9c87c43e23e9b3c5668a739f58e1666dcfddc18e
Mathieu Charron <mathieucharron42@gmail.com>
2021-11-08 23:21:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PotatoGathererEater
+A staging of potato gatherers and potato eaters
+Developed with Unreal Engine 4.27.1
---
+++ Source/PotatoGathererEater/PotatoGathererEaterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PotatoGathererEaterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APotatoGathererEaterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APotatoGathererEaterGameMode();
+};
+
+
+
