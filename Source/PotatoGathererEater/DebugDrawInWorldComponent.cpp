9fe57a4f69d074c23d17afb11f9925a50cadcbec
Mathieu Charron <mathieucharron42@gmail.com>
2022-02-21 01:26:13 -0500

    Add DebugDrawInWorldComponent to showcase a few in world draw

--- Source/PotatoGathererEater/DebugDrawInWorldComponent.cpp
+++ Source/PotatoGathererEater/DebugDrawInWorldComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "DebugDrawInWorldComponent.h"
 #include "DrawDebugHelpers.h"
+#include "Engine/Engine.h"
+#include "Engine/Font.h"
 
 UDebugDrawInWorldComponent::UDebugDrawInWorldComponent(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -21,10 +23,44 @@ void UDebugDrawInWorldComponent::TickComponent(float DeltaTime, ELevelTick TickT
 	UWorld* world = GetWorld();
 	if (IsValid(world))
 	{
+		const float DepthRenderingPriority = 100.f;
+
 		FVector position = GetComponentTransform().GetLocation();
+		FQuat rotation = GetComponentTransform().GetRotation();
+		AActor* parent = GetOwner();
+		FVector origin;
+		FVector parentSize;
+		parent->GetActorBounds(true, origin, parentSize);
+
 		if (_pointEnabled)
 		{
-			DrawDebugPoint(world, position, _pointSize, _pointColor, false, _pointLifeTime, _pointDepthRenderingPriority);
+			DrawDebugPoint(world, position, _pointProperties.Size, _pointProperties.Color, false, _pointProperties.Lifetime, DepthRenderingPriority);
+		}
+
+		if (_boxEnabled)
+		{
+			FQuat boxRotation = _boxProperties.InheritRotation ? rotation : FQuat::Identity;
+			DrawDebugBox(world, position, _boxProperties.Size, boxRotation, _boxProperties.Color, false, _boxProperties.Lifetime, DepthRenderingPriority, _boxProperties.Thickness);
+		}
+
+		if (_sphereEnabled)
+		{
+			DrawDebugSphere(world, position, _sphereProperties.Radius, _sphereProperties.NbSegments, _sphereProperties.Color, false, _sphereProperties.Lifetime, DepthRenderingPriority, _sphereProperties.Thickness);
+		}
+
+		if (_arrowEnabled)
+		{		
+			FVector begin = position;
+			FVector direction = _arrowProperties.InheritRotation ? rotation.RotateVector(_arrowProperties.Direction) : _arrowProperties.Direction;
+			FVector end = position + (direction.GetSafeNormal() * _arrowProperties.Lenght);
+			DrawDebugDirectionalArrow(world, position, end, _arrowProperties.Size, _arrowProperties.Color, false, _arrowProperties.Lifetime, DepthRenderingPriority, _arrowProperties.Thickness);
+		}
+
+		if (_textEnabled)
+		{
+			FVector textPosition = position + (_textProperties.InheritRotation ? rotation.RotateVector(_textProperties.Position) : _textProperties.Position);
+			
+			DrawDebugString(world, textPosition, _textProperties.Text, nullptr, _textProperties.Color, _textProperties.Lifetime, true, _textProperties.Size);
 		}
 	}
 }
--- Source/PotatoGathererEater/DebugDrawInWorldComponent.h
+++ Source/PotatoGathererEater/DebugDrawInWorldComponent.h
@@ -6,6 +6,117 @@
 #include "Components/SceneComponent.h"
 #include "DebugDrawInWorldComponent.generated.h"
 
+USTRUCT()
+struct FDrawInWorldPointProperties
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 Size = 5;
+
+	UPROPERTY(EditDefaultsOnly)
+	FColor Color = FColor::White;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Lifetime = -1.f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float DepthRenderingPriority = 100.f;
+};
+
+USTRUCT()
+struct FDrawInWorldBoxProperties
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector Size = FVector(50.f, 50.f, 50.f);
+
+	UPROPERTY(EditDefaultsOnly)
+	bool InheritRotation;
+
+	UPROPERTY(EditDefaultsOnly)
+	FColor Color = FColor::White;
+
+	UPROPERTY(EditDefaultsOnly);
+	float Thickness = 2.0f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Lifetime = -1.f;
+};
+
+USTRUCT()
+struct FDrawInWorldSphereProperties
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	float Radius = 50.f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float NbSegments = 10;
+
+	UPROPERTY(EditDefaultsOnly)
+	FColor Color = FColor::White;
+
+	UPROPERTY(EditDefaultsOnly);
+	float Thickness = 2.0f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Lifetime = 0.f;
+};
+
+USTRUCT()
+struct FDrawInWorldArrowProperties
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector Direction = FVector::XAxisVector;
+
+	UPROPERTY(EditDefaultsOnly)
+	bool InheritRotation = true;
+
+	UPROPERTY(EditDefaultsOnly);
+	float Lenght = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly);
+	float Thickness = 2.0f;
+
+	UPROPERTY(EditDefaultsOnly);
+	float Size = 20.0f;
+
+	UPROPERTY(EditDefaultsOnly)
+	FColor Color = FColor::White;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Lifetime = 0.f;
+};
+
+USTRUCT()
+struct FDrawInWorldTextProperties
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector Position;
+
+	UPROPERTY(EditDefaultsOnly)
+	bool InheritRotation;
+
+	UPROPERTY(EditDefaultsOnly);
+	float Size = 1.0f;
+
+	UPROPERTY(EditDefaultsOnly)
+	FString Text;
+
+	UPROPERTY(EditDefaultsOnly)
+	FColor Color = FColor::White;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Lifetime = 0.f;
+};
+
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class POTATOGATHEREREATER_API UDebugDrawInWorldComponent : public USceneComponent
 {
@@ -18,18 +129,33 @@ private:
 	virtual void BeginPlay() override;
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	
-	UPROPERTY(EditDefaultsOnly, Category = "Point Debug", meta = (AllowPrivateAccess = true))
+	UPROPERTY(EditDefaultsOnly, Category = "Debug Point", meta = (AllowPrivateAccess = true))
 	bool _pointEnabled = false;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Point Debug", meta = (AllowPrivateAccess = true))
-	int32 _pointSize = 5;
+	UPROPERTY(EditDefaultsOnly, Category = "Debug Point", meta = (AllowPrivateAccess = true, EditCondition = "_pointEnabled"))
+	FDrawInWorldPointProperties _pointProperties;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Point Debug", meta = (AllowPrivateAccess = true))
-	FColor _pointColor = FColor::Blue;
+	UPROPERTY(EditDefaultsOnly, Category = "Debug Sphere", meta = (AllowPrivateAccess = true))
+	bool _sphereEnabled = false;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Point Debug", meta = (AllowPrivateAccess = true))
-	float _pointLifeTime = 1.f;
+	UPROPERTY(EditDefaultsOnly, Category = "Debug Sphere", meta = (AllowPrivateAccess = true, EditCondition = "_sphereEnabled"))
+	FDrawInWorldSphereProperties _sphereProperties;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Point Debug", meta = (AllowPrivateAccess = true))
-	float _pointDepthRenderingPriority = 100.f;
+	UPROPERTY(EditDefaultsOnly, Category = "Debug Box", meta = (AllowPrivateAccess = true))
+	bool _boxEnabled = false;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Debug Box", meta = (AllowPrivateAccess = true, EditCondition = "_boxEnabled"))
+	FDrawInWorldBoxProperties _boxProperties;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Debug Arrow", meta = (AllowPrivateAccess = true))
+	bool _arrowEnabled = false;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Debug Arrow", meta = (AllowPrivateAccess = true, EditCondition = "_arrowEnabled"))
+	FDrawInWorldArrowProperties _arrowProperties;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Debug Text", meta = (AllowPrivateAccess = true))
+	bool _textEnabled = false;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Debug Text", meta = (AllowPrivateAccess = true, EditCondition = "_textEnabled"))
+	FDrawInWorldTextProperties _textProperties;
 };
